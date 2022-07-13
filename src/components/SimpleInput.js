ef92171051abc899a344b82dc06e74535bc1ac50
Atanasije Rankovic <atanasije.rankovic@gmail.com>
2022-07-13 18:19:13 +0200

    providing feedback, improving styling of input field

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,6 +3,7 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
+  const [enteredNameIsValid, setEnteredNameisValid] = useState(true);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -12,9 +13,11 @@ const SimpleInput = (props) => {
     event.preventDefault();
 
     if(enteredName.trim() == '') {
+      setEnteredNameisValid(false);
       return;
     }
 
+    setEnteredNameisValid(true);
     console.log(enteredName);
 
     const enteredValue = nameInputRef.current.value; 
@@ -25,9 +28,11 @@ const SimpleInput = (props) => {
     setEnteredName("");
   };
 
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid';
+
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className="form-control">
+      <div className={nameInputClasses}>
         <label htmlFor="name">Your Name</label>
         <input
           ref={nameInputRef}
@@ -36,6 +41,7 @@ const SimpleInput = (props) => {
           onChange={nameInputChangeHandler}
           value={enteredName}
         />
+        {!enteredNameIsValid && <p className="error-text">Name input must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
